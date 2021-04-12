47911d1dd3df88312d784fa18f97f25182ca93f2
maulid246 <suwung246@gmail.com>
2021-04-12 12:27:03 +0700

    first commit

---
+++ helloworld/src/components/nama.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1> {{msg}} </h1>
+    </div>
+</template>
+
+<style scoped>
+h1 {
+    color:aqua;
+    padding: 10px 50px;
+    background-color:azure;
+
+}
+</style>
+<script>
+export default {
+   name: "nama",
+   props: {
+       msg : String
+   }
+   
+}
+</script>
